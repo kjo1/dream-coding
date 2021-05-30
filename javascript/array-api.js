d7ee181c6432ef3f9a52fddc7d8004994b343396
Kanghyun Jo <kjo0@outlook.com>
2021-05-30 19:45:52 -0400

    Do the array quizzes again with the tutorial

--- javascript/array-api.js
+++ javascript/array-api.js
@@ -53,27 +53,57 @@ const students = [new Student('A', 29, true, 45), new Student('B', 28, false, 80
 
 // Q6. make an array of enrolled students
 {
+	const result = students.filter((student) => student.enrolled);
+	console.log(result);
 }
 
 // Q7. make an array containing only the students' scores
 // result should be: [45, 80, 90, 66, 88]
 {
+	const result = students.map((student) => student.score);
+	console.log(result);
 }
 
 // Q8. check if there is a student with the score lower than 50
 {
+	const result = students.some((student) => student.score < 50);
+	console.log(result);
+
+	const result2 = !students.every((student) => student.score >= 50);
+	console.log(result2);
 }
 
+console.clear();
 // Q9. compute students' average score
 {
+	// * before simplified
+	// const result = students.reduce((prev, curr) => {
+	// 	console.log('-----------------------------');
+	// 	console.log(prev);
+	// 	console.log(curr);
+	// 	return prev + curr.score;
+	// }, 0);
+
+	const result = students.reduce((prev, curr) => prev + curr.score, 0);
+	console.log(result / students.length);
 }
 
 // Q10. make a string containing all the scores
 // result should be: '45, 80, 90, 66, 88'
 {
+	const result = students
+		.map((student) => student.score)
+		.filter((score) => score >= 50)
+		.join();
+	console.log(result);
 }
 
 // Bonus! do Q10 sorted in ascending order
 // result should be: '45, 66, 80, 88, 90'
 {
+	const result = students
+		.map((student) => student.score)
+		.sort((a, b) => a - b)
+		.join();
+	console.log(result);
 }
