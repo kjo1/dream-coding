392fd6c37ebc2ef8d27d69b4840583c5b8d9f901
Kanghyun Jo <kjo0@outlook.com>
2021-06-25 06:22:02 -0400

    Basic variable

---
+++ javascript/variable-basic.js
@@ -0,0 +1,29 @@
+// // number, string. boolean, null, undefined
+// let number = 2;
+// let number2 = number;
+
+// console.log(number);
+// console.log(number2);
+
+// number2 = 3;
+
+// console.log(number);
+// console.log(number2);
+
+// object
+// *although you declare an object as const you can change the value because references are still not changing
+const obj = {
+	name: 'sonsy',
+	age: 5
+};
+// console.log(obj.name);
+
+// let obj2 = obj;
+// console.log(obj2.name);
+
+// obj.name = 'ellie';
+// console.log('---------------');
+// console.log(obj.name);
+// console.log(obj2.name);
+
+obj.name = 'Ellie';
